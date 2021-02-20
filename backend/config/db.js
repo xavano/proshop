d2a0aba182b0a715efa9499353ae95efcb25e25e
Xavano <p.a.medak@gmail.com>
2021-02-20 15:06:30 +0000

    add production database destintion

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,12 +2,11 @@ import mongoose from 'mongoose'
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-      useCreateIndex: true,
-    })
-
+      const conn = await mongoose.connect(process.env.NODE_ENV === 'production' ? process.env.MONGO_PRODUCTION : process.env.MONGO_URI, {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+        useCreateIndex: true,
+      });
     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
   } catch (error) {
     console.error(`Error: ${error.message}`.red.underline.bold)
