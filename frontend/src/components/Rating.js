2291a55c5b238d1066854fb509ed11ecea100cd4
Xavano <p.a.medak@gmail.com>
2021-02-20 14:31:50 +0000

    first deploy

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -10,7 +10,7 @@ const Rating = ({ value, text, color }) => {
           <i style={{ color }} className={ (value >= index) ? 'fas fa-star' : ((value >= (index - 0.5)) ? 'fas fa-star-half-alt' : 'far fa-star')}></i>
         </span>
       )) }
-      <span>{text && text}</span>
+      <span className="reviews">{text && text}</span>
     </div>
 
     // <div className='rating'>
